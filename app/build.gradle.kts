8523ff409555b1061e1103d333aa78f0b6ab5833
nhchoi2 <choi1278@gmail.com>
2025-04-11 00:09:44 +0900

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
 
     // Firebase BoM (Bill of Materials)
     implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
-
+    implementation("com.google.android.material:material:1.4.0")
     // Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
